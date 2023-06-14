3ad0fe79d626ccdb3498ff730c16ea552f3e0783
axcd <maoyuyin@126.com>
2023-06-14 20:36:58 +0800

    first commit

---
+++ jni/Android.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .c
+LOCAL_MODULE    := native-activity
+LOCAL_SRC_FILES := main.c
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
+
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
+ifeq ($(TARGET_ARCH_ABI),x86)
+    LOCAL_CFLAGS += -ffast-math -mtune=atom -mssse3 -mfpmath=sse
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+#include /storage/emulated/0/AppProjects/MyNDKApp/jni/android/native_app_glue/Android.mk
+$(call import-module,android/native_app_glue)
